c5b5a853fca21fe428c400d31fe97bf0994cf500
Evgeny Cherkashin <eugeneai@irnok.net>
2015-03-02 12:40:22 +0800

    Adjusted some configuration.

--- copy-win.sh
+++ copy-win.sh
@@ -16,10 +16,10 @@ DIR22="$DIR/VirtualBox_VMs"
 
 ssh-copy-id guest@$IP
 $DEBUG ssh root@$IP chown guest:users -R $DIR1
-$DEBUG rsync -u $DIR1/VirtualBox.xml* $U:/$DIR1
+# $DEBUG rsync -u $DIR1/VirtualBox.xml* $U:/$DIR1
 $DEBUG ssh root@$IP chown guest:users -R $DIR21
 $DEBUG ssh root@$IP mv $DIR21 $DIR22
 $DEBUG mv $DIR2 $DIR22
-$DEBUG rsync --progress --stats -arz $DIR22/VirtualXP $U:$DIR22
+$DEBUG rsync --progress --stats -arzu $DIR22/VirtualXP $U:$DIR22
 $DEBUG ssh root@$IP mv $DIR22 $DIR21
 $DEBUG mv $DIR22 $DIR2
