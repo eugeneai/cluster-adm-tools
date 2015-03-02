215bb88323ec2730c1a3db83968461415fb25376
Evgeny Cherkashin <eugeneai@irnok.net>
2015-03-02 12:28:10 +0800

    Virtual box windows image copying tool.

---
+++ copy-win.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IFS='
+'
+
+IP=$1
+U=guest@$IP
+
+# DEBUG=echo
+
+DIR="/home/guest"
+DIR1="$DIR/.VirtualBox"
+DIR2="$DIR/VirtualBox VMs"
+DIR21="$DIR/VirtualBox\ VMs"
+DIR22="$DIR/VirtualBox_VMs"
+
+ssh-copy-id guest@$IP
+$DEBUG ssh root@$IP chown guest:users -R $DIR1
+$DEBUG rsync -u $DIR1/VirtualBox.xml* $U:/$DIR1
+$DEBUG ssh root@$IP chown guest:users -R $DIR21
+$DEBUG ssh root@$IP mv $DIR21 $DIR22
+$DEBUG mv $DIR2 $DIR22
+$DEBUG rsync --progress --stats -arz $DIR22/VirtualXP $U:$DIR22
+$DEBUG ssh root@$IP mv $DIR22 $DIR21
+$DEBUG mv $DIR22 $DIR2
